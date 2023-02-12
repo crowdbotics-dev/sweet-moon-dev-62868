4a4f81f1f108fe3df5c838399d153cd643d3a2ac
Crowdbotics <engineering@crowdbotics.com>
2023-02-12 00:04:55 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/sweet_moon_dev_62868/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sweet_moon_dev_62868;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "sweet_moon_dev_62868";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sweet-moon-dev-62868",
+  url: "https://sweet-moon-dev-62868.botics.co",
+  api: "https://sweet-moon-dev-62868.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
